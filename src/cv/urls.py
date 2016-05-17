a011d8d0388a28b63aeecd2b31e4579adb3d4170
kei-cs <keisuke.cs@gmail.com>
2016-05-17 22:27:45 +0900

    Tried reportlab with django

--- src/cv/urls.py
+++ src/cv/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^cv_form/$', views.test, name = 'cv_form' ),
+    url(r'^cv_generate/$', views.test_generate, name = 'cv_generate'),
 ]
--- src/cv/views.py
+++ src/cv/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from reportlab.pdfgen import canvas
+
 
 def test(request):
     template = 'cv/test_form.html'
     return render(request, template, {})
+
+def test_generate(request):
+    response = HttpResponse(content_type = 'application/pdf')
+    response['Content-Dispotion'] = 'filename="test.pdf"'
+
+    pdf = canvas.Canvas(response)
+    pdf.drawString(100, 100, 'Hello, World')
+
+    pdf.showPage()
+    pdf.save()
+
+    return response
