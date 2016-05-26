45f47ca9a2a8f5c9d530bfc85aee4f84fa9e387c
kei-cs <keisuke.cs@gmail.com>
2016-05-26 20:53:49 +0900

    Added cv/admin.py

--- src/cv/admin.py
+++ src/cv/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .forms import BasicInfoForm
+from .models import (
+    BasicInfo,
+    Education,
+    WorkExperience,
+    Certification,
+    Reason,
+    Question,
+    ExtraInfo,
+    CV,
+    CVTemplate,
+)
+
+
+class BasicInfoAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'updated', 'created']
+    form  = BasicInfoForm
+
+
+admin.site.register(BasicInfo, BasicInfoAdmin)
+admin.site.register(Education)
+admin.site.register(WorkExperience)
+admin.site.register(Certification)
+admin.site.register(Reason)
+admin.site.register(Question)
+admin.site.register(ExtraInfo)
+admin.site.register(CV)
+admin.site.register(CVTemplate)
